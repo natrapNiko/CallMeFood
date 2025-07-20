336f6e43f0c4af85ee921a7afdbf5a0703512524
natrapNiko <natrapniko@gmail.com>
2025-07-20 14:09:40 +0200

    feat(services): implement GetAllAsync method in RecipeService and wire up RecipeController Index action to display all recipes

---
+++ CallMeFood.Services/RecipeService.cs
@@ -0,0 +1,86 @@
+﻿
+namespace CallMeFood.Services
+{
+    using CallMeFood.Data;
+    using CallMeFood.Data.Models;
+    using CallMeFood.Services.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RecipeService : IRecipeService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecipeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // get all recipes
+        public async Task<IEnumerable<Recipe>> GetAllAsync()
+        {
+            return await _context.Recipes
+                .Include(r => r.Category)
+                .Include(r => r.User)
+                .Include(r => r.Ingredients)
+                .Include(r => r.Comments)
+                .Include(r => r.Favorites)
+                .ToListAsync();
+        }
+
+        public Task AddAsync(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<Recipe> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> GetByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> GetLatestAsync(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> GetMostFavoritedAsync(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> GetPagedAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetTotalCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> SearchByCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> SearchByTitleAsync(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CallMeFood/Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CallMeFood.Web.Controllers
+{
+    using CallMeFood.Services.Interfaces;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RecipeController : Controller
+    {
+        private readonly IRecipeService _recipeService;
+
+        public RecipeController(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        // GET: Recipe
+        public async Task<IActionResult> Index()
+        {
+            var recipes = await _recipeService.GetAllAsync();
+            return View(recipes);
+        }
+    }
+}
